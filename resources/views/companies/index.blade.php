795681a6c615de0e60721d13d767c65e522bf2c2
zenbuduh <zenbuduh@gmail.com>
2020-02-12 00:41:25 +0100

    added dynamic company retrieval view

---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+<div class="col-md-6 mx-auto col-lg-6">
+    <div class="card ">
+        <div class="card-header text-center bg-primary text-white">
+            <h3>Companies</h3>
+        </div>
+        <div class="card-body">
+            <!-- <h5 class="card-title">Special title treatment</h5> -->
+            <ul class="list-group list-striped">
+                @foreach($companies as $company)
+                <li class="list-group-item">{{ $company->name }}</li>
+                @endforeach
+            </ul>
+        </div>
+        <!-- <div class="card-footer text-muted">
+            2 days ago
+        </div> -->
+    </div>
+</div>
+@endsection
