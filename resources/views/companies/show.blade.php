36648957ec7b5fbade4ec9641470c22547f520dc
Zenahr Barzani <zenmatica@gmail.com>
2020-02-12 17:23:45 +0100

    added edit view for companies

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -4,32 +4,48 @@
     <h1 class="display-3">{{ $company->name }}</h1>
     <p class="lead">{{ $company->description }}</p>
 </div>
-
+<!-- Page Content -->
 <div class="container">
-    <h1>Projects</h1>
-    <ul class="list-group list-striped">
-        @foreach($company->projects as $project)
-        <li class="list-group-item">
-            <h4>{{ $project->name }}</h4>
-        </li>
-        <li class="list-group-item mb-4">
-            <h6>{{ $project->description }}</h6>
-        </li>
-        <a name="" id="" class="btn btn-primary" href="/project/{{ $project->id }}" role="button">View Details</a>
-        @endforeach
-    </ul>
+    <div class="row">
+        <!-- Post Content Column -->
+        <div class="col-lg-8">
+            <div class="card my-4">
+                <h1 class="card-header">Projects</h1>
+                <div class="card-body">
+                    @foreach($company->projects as $project)
+                    <li class="list-group-item">
+                        <h4>{{ $project->name }}</h4>
+                    </li>
+                    <li class="list-group-item mb-4">
+                        <h6>{{ $project->description }}</h6>
+                        <a name="" id="" class="btn btn-primary" href="/project/{{ $project->id }}" role="button">View
+                            Details</a>
+                    </li>
+                    @endforeach
+                </div>
+            </div>
+
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-4">
+
+            <!-- Side Widget -->
+            <div class="card my-4">
+                <h5 class="card-header">CRUD Actions</h5>
+                <div class="card-body">
+                    <ol class="list-unstyled">
+                        <li><a href="/companies/{{ $company->id  }}/edit">Edit</a></li>
+                        <li><a href="">Delete</a></li>
+                        <li><a href="">Add Member</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+
 </div>
-
-
-
-
-<div class="col-sm-3 col-md-3 col-lg-3">
-        <ol class="list-unstyled">
-        <h4>CRUD Actions</h4>
-            <li><a href="">Edit</a></li>
-            <li><a href="">Delete</a></li>
-            <li><a href="">Add Member</a></li>
-        </ol>
-</div>
-
+<!-- /.container -->
 @endsection
